e593d214f1204cb7325956339907f37440405067
Adrian Chin <syncing95@gmail.com>
2020-12-27 22:29:46 +0800

    refactor allow cors middleware

--- src/app.js
+++ src/app.js
@@ -1,18 +1,14 @@
 const express = require('express')
 require('./db/mongoose.js')
 const userRouter = require('./routers/user')
+const allowCors = require('./middleware/allow-cors')
 // const taskRouter = require('./routers/task')
 
 const app = express()
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept');
-    next();
-});
+app.use(allowCors);
 app.use(express.json())
 app.use(userRouter)
 // app.use(taskRouter)
 
 module.exports = app
-
