fcd4ad47b6c835145485d22fa6d3bf87111aaa56
Adrian Chin <syncing95@gmail.com>
2020-12-30 15:43:21 +0800

    add cors dependency

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express')
 require('./db/mongoose.js')
 const userRouter = require('./routers/user')
 const allowCors = require('./middleware/allow-cors')
+const cors = require('cors');
 const postRouter = require('./routers/post')
 
 const app = express()
 
-app.use(allowCors);
+app.use(cors())
+// app.use(allowCors);
 app.use(express.json())
 app.use(userRouter)
 app.use(postRouter)
